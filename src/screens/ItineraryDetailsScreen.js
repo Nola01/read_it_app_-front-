1d6b4540070b11cde69d14e154789dddc69dbda6
Nola01 <juanmanolasco0105@gmail.com>
2022-05-23 16:52:47 +0200

    Add slide dot

--- src/screens/ItineraryDetailsScreen.js
+++ src/screens/ItineraryDetailsScreen.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {Dimensions, Text, StyleSheet, ScrollView, Pressable} from 'react-native';
+import {Dimensions, Text, StyleSheet, ScrollView, Pressable, View} from 'react-native';
 import {Card} from 'react-native-paper';
 
 import Carousel from '../components/ImagesCarousel';
@@ -8,7 +8,20 @@ import Carousel from '../components/ImagesCarousel';
 const screenWidth = Dimensions.get("window").width;
 
 const ItineraryDetailsScreen = ({route, navigation}) => {
+
+    // const state = {
+    //     active: 0
+    // }
+
+    // const change = ({nativeEvent}) => {
+    //     const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+    //     if (slide !== state.active) {
+    //         state.active = slide;
+    //     }
+    // }
+
     const item = route.params;
+    const books = item.books;
 
     const images = []; 
     item.books.forEach(book => {
@@ -26,17 +39,29 @@ const ItineraryDetailsScreen = ({route, navigation}) => {
     <ScrollView style={styles.viewBody}>
         <Card style={styles.item}>
             <Card.Title title={item.name} subtitle={`Departamento: ${item.department}`} />
-            <Pressable onPress={() => goToDetails(item)}>
-                <Carousel 
-                    images={images}
-                    height={700}
-                    width={screenWidth}
-                />
-            </Pressable>
-            <Card.Content>
+            <Card.Content style={styles.content}>
                 <Text style={styles.text}>Libros: {item.books.length}</Text>
                 <Text style={styles.text}>{item.students[0].name}</Text>
             </Card.Content>
+            <Pressable onPress={() => goToDetails(item)}>
+                <Card.Title title='Ver detalles de los libros >' onPress={() => goToDetails(item)} />
+            </Pressable>
+            <View style={styles.pagination}>
+                {
+                    books.map(book => {
+                        //console.log(book);
+                        // return <Text key={book.isbn} style={book.isbn==state.active ? styles.paginationActiveDot : styles.paginationDot}>⬤</Text>
+                        return <Text key={book.isbn} style={styles.paginationDot}>⬤</Text>
+                    })
+                }
+            </View>
+
+            <Carousel 
+                images={images}
+                height={600}
+                width={screenWidth}
+            />
+            
         </Card>
     </ScrollView>
     );
@@ -57,11 +82,27 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         color: 'black',
     },
+    content: {
+        marginBottom: 10
+    },
     cover: {
         width: '50%',
         height: '60%'
     },
     viewBody: {
         flex: 1
+    },
+    pagination: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        color: 'black'
+    },
+    paginationDot: {
+        color: '#888',
+        margin: 3
+    },
+    paginationActiveDot: {
+        color: '#fff',
+        margin: 3
     }
 });
